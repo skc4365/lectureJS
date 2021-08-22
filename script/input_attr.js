4d43ffae0081ac02d3e6c1a5f8428416fff0e517
Sun-Kyoung <skc4365@gmail.com>
2021-08-22 22:31:09 +0900

    JS 데이터출력

---
+++ script/input_attr.js
@@ -0,0 +1,33 @@
+// 선택자를 변수에 담아서 사용해본다.
+let btn = document.getElementById("btn");
+let btnStyle = document.getElementById("btn").style;
+
+function getDisabled() {
+    btn.disabled = true;
+    btnStyle.cursor = "none";
+    btnStyle.background = "initial";
+}
+function getAbled() {
+    btn.disabled = false;
+    btnStyle.cursor = "pointer";
+    btnStyle.background = "pink";
+}
+
+// 변수의 값은 콘솔에서 확인한다.
+let result;
+
+function getType() {
+    result = "타입: " + document.getElementById("btn").type;
+}
+function getId() {
+    result = "아이디: " + document.getElementById("btn").id;
+}
+function getClass() {
+    result = "클래스: " + document.getElementById("btn").className;
+}
+function getName() {
+    result = "이름: " + document.getElementById("btn").name;
+}
+function getValue() {
+    result = "값: " + document.getElementById("btn").value;
+}
---
+++ script/output.js
@@ -0,0 +1,13 @@
+// html요소로 출력 작성
+document.getElementById("out").innerHTML 
+= "<pre>// html요소로 출력 작성\ndocument.getElementById(\"demo\").innerHTML</pre>";
+
+// doucument 출력으로 작성
+document.write("<pre>// doucument 출력으로 작성\ndocument.write()</pre>");
+
+// window객체 경고창을 사용하여 출력
+window.alert("// window객체 경고창을 사용하여 출력\nwindow.alert()");
+
+// 브라우저 콘솔을 사용하여 출력.
+let logWrite = "// 브라우저 콘솔을 사용하여 출력 =>> console.log()"
+console.log(logWrite);
